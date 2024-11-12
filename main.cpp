d297116017aaff2ed4b3940245068bb9161ef436
kavyas21-co <kavya.s.raja@gmail.com>
2024-11-12 15:34:05 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(){
+    std::cout << "Hello, World!" << std::endl;
+     std::cout << "world" << std::endl;
+      std::cout << "world 123" << std::endl;
+      std::cout << " 123" << std::endl;
+    return 0;
+}
